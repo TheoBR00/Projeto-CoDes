297f37902422117611bc9712f27325dbfd84cd4b
mauricioha <38228116+mauricioha@users.noreply.github.com>
2018-09-26 07:26:35 -0300

    Maurício
    Limpei o código menu.js

--- assets/java/menu.js
+++ assets/java/menu.js
@@ -3,15 +3,12 @@ document.addEventListener('DOMContentLoaded', function(){
 	let lista = document.getElementsByClassName("lista")
 	
 	let fecha = document.getElementsByClassName("fecha")
-	for(let item of fecha){
-		item.style.visibility = "hidden"
-	}
 	
 	let rotulo = document.querySelectorAll('.rotulo')
 
 	for(let i=0;i<rotulo.length;i++){
 		rotulo[i].addEventListener('click',function(){
-			setTimeout(10,document.getElementsByClassName("lista")[i].style.visibility = "visible")
+			document.getElementsByClassName("lista")[i].style.visibility = "visible"
 			document.getElementsByClassName("fecha")[i].style.visibility = "visible"
 		})
 		fecha[i].addEventListener('click',function(){
